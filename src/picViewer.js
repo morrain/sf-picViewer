26d975721d4bcd0684c37c7c7831e7c91ddc552a
kongchuiliang <hzkongchuiliang@netease.corp.com>
2017-02-27 14:36:25 +0800

    图片查看器组件。上传ysfman run自动生成的文件

---
+++ src/picViewer.js
@@ -0,0 +1,25 @@
+/**
+ * sf-picViewer 组件
+ *
+ * @author:   morrain
+ * @date:     Feb 27, 2017 2:30 PM
+ *
+ * @props  {String} prop         -参数         
+ * 
+ */
+
+require('./picViewer.scss');
+
+module.exports = {
+	name : 'sf-picViewer',
+	template : require('./picViewer.html'),
+	props : {
+
+	},
+	data : function() {
+		return {}
+	},
+	methods : {
+		
+	}
+}
